582e1afbbba2901c8bd9a44f8ce2845472a6a289
SLOBY <szabolcs.maj@gmail.com>
2013-12-25 23:49:20 +0100

    Added "secure installer" which creates a more secure glassfish instance. Installer not made entirely for vagrant instances but rather for real servers

--- glassfish/glassfish-secure.sh
+++ glassfish/glassfish-secure.sh
@@ -5,17 +5,13 @@
 GF_UNZIP_TARGET="/opt/"
 ASADMIN="${GF_UNZIP_TARGET}glassfish/bin/asadmin"
 PASSWORD_FILE="/opt/glassfish/password.file"
-ADMIN_PASSWORD="FmxGlassfishAdmin1"
-MASTER_PASSWORD="FmxGlassfishMaster1"
-PASSWORD="vagrant"
-SERVER_DOMAIN_NAME="vps.dev"
-ORGANIZATION_UNIT="SomeOU"
-ORGANIZATION="SomeO"
-LOCATION="Gyor"
-STATE="GY-M-S"
-COUNTRY="HU"
+ADMIN_PASSWORD="adminPassword"
+MASTER_PASSWORD="masterPassword"
+GLASSFISH_PATH="/opt/glassfish/glassfish"
 
 setupUsers() {
+	# Create a system level glasfish user and group. System user means that UID 
+	# and GID are below 1000
 	adduser --system --group --home /home/glassfish --shell /bin/bash glassfish
 }
 
@@ -38,7 +34,7 @@ setupEnvVariables() { # Set java env variables
 	echo "export AS_JAVA=$JAVA_HOME" >> $BASHRC_FILE
 }
 
-bootstrap_system() {
+bootstrapSystem() {
 	sudo apt-get -y -q update
 	sudo apt-get -y -q install unzip vim openjdk-7-jdk 
 }
@@ -58,7 +54,7 @@ getGlassfish() {
 	fi
 }
 
-unpack_glassfish() {
+unpackGlassfish() {
 	unzip /tmp/glassfish-4.0.zip -d ${GF_UNZIP_TARGET}
 	# Rename the zip
 	mv $GF_UNZIP_TARGET/glassfish4 $GF_UNZIP_TARGET/glassfish
@@ -80,7 +76,6 @@ modifyFolderPermissions() {
 }
 
 changePasswords() {
-	GLASSFISH_PATH=/opt/glassfish/glassfish
 	su - glassfish -c "tar cf passwords.orig.tar $GLASSFISH_PATH/domains/domain1/config/domain-passwords $GLASSFISH_PATH/domains/domain1/config/keystore.jks $GLASSFISH_PATH/domains/domain1/config/cacerts.jks"
 
 	# Change default master password
@@ -100,11 +95,18 @@ changePasswords() {
 }
 
 generatePassFile() {
-	# Login, thus create a file ~/.gfclient/pass storgin the login information
+	# Login, thus create a file ~/.gfclient/pass storing the login information
+	# NOT WORKING --> It does not accept piped answers for interactive questions
 	su - glassfish -c "echo -e \"\nadmin$ADMIN_PASSWORD\n\" | $ASADMIN login"
 }
 
 updateCertificates() {
+	SERVER_DOMAIN_NAME="vps.dev"
+	ORGANIZATION_UNIT="SomeOrganizationUnit"
+	ORGANIZATION="SomeOrganization"
+	LOCATION="SomeLocation"
+	STATE="SomeState"
+	COUNTRY="HU"
 	# The certificates (namely "s1as" and "glassfish-instance") are default, 
 	# therefore known by everyone. It's unsecure so we change them
 	KEYSTORE_PATH=$GLASSFISH_PATH/domains/domain1/config/keystore.jks
@@ -122,7 +124,7 @@ updateCertificates() {
 	su - glassfish -c "keytool -exportcert -alias s1as -file $GLASSFISH_PATH/s1as.cert -keystore $KEYSTORE_PATH -storepass $MASTER_PASSWORD"
 	su - glassfish -c "keytool -exportcert -alias glassfish-instance -file $GLASSFISH_PATH/glassfish-instance.cert -keystore $KEYSTORE_PATH -storepass $MASTER_PASSWORD"
 
-	# Update cacerts.jks
+	# Update cacerts.jks with the exported certificates
 	CACERT_PATH=$GLASSFISH_PATH/domains/domain1/config/cacerts.jks
 	su - glassfish -c "keytool -delete -alias s1as -keystore $CACERT_PATH -storepass $MASTER_PASSWORD"
 	su - glassfish -c "keytool -delete -alias glassfish-instance -keystore $CACERT_PATH -storepass $MASTER_PASSWORD"
@@ -134,25 +136,31 @@ updateCertificates() {
 }
 
 enableRemoteAccess() {
-	su - glassfish -c "$ASADMIN set server-config.network-config.protocols.protocol.admin-listener.security-enabled=true"
-	su - glassfish -c "$ASADMIN enable-secure-admin"
+	su - glassfish -c "$ASADMIN --user admin --passwordfile $PASSWORD_FILE set server-config.network-config.protocols.protocol.admin-listener.security-enabled=true"
+	su - glassfish -c "$ASADMIN --user admin --passwordfile $PASSWORD_FILE enable-secure-admin"
+}
+
+restartGlassfish() {
+	$ASADMIN --user admin --passwordfile $PASSWORD_FILE stop-domain
+	$ASADMIN --user admin --passwordfile $PASSWORD_FILE start-domain
 }
 
 obfuscateHTMLHeaders() {
-	su - glassfish -c "$ASADMIN create-jvm-options -Dproduct.name="
-	su - glassfish -c "$ASADMIN set server.network-config.protocols.protocol.http-listener-1.http.xpowered-by=false"
-	su - glassfish -c "$ASADMIN set server.network-config.protocols.protocol.http-listener-2.http.xpowered-by=false"
-	su - glassfish -c "$ASADMIN set server.network-config.protocols.protocol.admin-listener-2.http.xpowered-by=false"
+	# Removes HTML headers like "X-Powered-by" signaling the client that it's a glassfish server
+	su - glassfish -c "$ASADMIN --user admin --passwordfile $PASSWORD_FILE create-jvm-options -Dproduct.name="
+	su - glassfish -c "$ASADMIN --user admin --passwordfile $PASSWORD_FILE set server.network-config.protocols.protocol.http-listener-1.http.xpowered-by=false"
+	su - glassfish -c "$ASADMIN --user admin --passwordfile $PASSWORD_FILE set server.network-config.protocols.protocol.http-listener-2.http.xpowered-by=false"
+	su - glassfish -c "$ASADMIN --user admin --passwordfile $PASSWORD_FILE set server.network-config.protocols.protocol.admin-listener.http.xpowered-by=false"
 }
 
 setupUsers
-bootstrap_system
+bootstrapSystem
 setupEnvVariables
 getGlassfish
-unpack_glassfish
+unpackGlassfish
 modifyFolderPermissions
 changePasswords
-generatePassFile
+#generatePassFile
 updateCertificates
 enableRemoteAccess
 obfuscateHTMLHeaders
